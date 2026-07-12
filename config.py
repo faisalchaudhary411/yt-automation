7d7cf67d16b7713af582d813c8f7a1c4970f3186
Faisal Aslam <faisalchaudhary411@gmail.com>
2026-07-12 14:23:18 +0500

    Update config.py

--- config.py
+++ config.py
@@ -20,8 +20,9 @@ import base64
 import requests
 
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY", "")
-GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY","")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 PEXELS_API_KEY = os.environ.get("PEXELS_API_KEY", "")
+OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY", "")
 
 CHANNEL_NAME = os.environ.get("CHANNEL_NAME", "WealthThroughAges")
 
