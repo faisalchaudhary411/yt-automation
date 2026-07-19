bba80ee5d1c2661aa7f20667bfafbe58c01b001f
Faisal Aslam <faisalchaudhary411@gmail.com>
2026-07-19 13:12:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -882,9 +882,7 @@ def oauth2callback():
         )
     return build_callback_html(tokens)
 
-
     return "YouTube channel connected and token verified in GitHub. You can close this tab and return to the app."
-
 
 @app.route("/approve/<video_id>")
 def approve(video_id):
